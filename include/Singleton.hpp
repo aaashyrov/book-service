5207e6f8e109fbed9b81842f251a7b79b25d7e72
alisher_ashyrov <alisher_ashyrov@mail.ru>
2022-01-09 02:04:05 +0300

    feat: added Singleton class

---
+++ include/Singleton.hpp
@@ -0,0 +1,38 @@
+//
+// Created by alisher on 1/9/22.
+//
+
+#include <atomic>
+#include <mutex>
+
+template <typename T>
+class Singleton {
+   protected:
+    Singleton() = default;
+
+   public:
+    Singleton(Singleton const&) = delete;
+    Singleton(Singleton&&) noexcept = default;
+    Singleton& operator=(Singleton const&) = delete;
+    Singleton& operator=(Singleton&&) noexcept = default;
+
+   public:
+    template <typename... Args>
+    static T* instance(Args... args) noexcept {
+        static std::mutex mutex;
+        static std::atomic<T*> instance;
+
+        auto isLoaded = instance.load() != nullptr;
+        if (not isLoaded) {
+            std::lock_guard _ {mutex};
+
+            isLoaded = instance.load() != nullptr;
+            if (not isLoaded) {
+                instance = new T(args...);
+            }
+        }
+
+        return instance;
+    }
+};
+
