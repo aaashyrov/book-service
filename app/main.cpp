bd891b58efd9d3e621ecf5b21ef185d88c09ac65
alisher_ashyrov <alisher_ashyrov@mail.ru>
2022-01-09 02:34:41 +0300

    feat: update

--- app/main.cpp
+++ app/main.cpp
@@ -2,7 +2,13 @@
 // Created by alisher on 1/9/22.
 //
 #include <iostream>
+#include <drogon/drogon.h>
 
 int main(int argc, char** argv){
-    return EXIT_SUCCESS
+    if(argc<2){
+        std::cerr<< argv[0]<<" <config file name>\n";
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
