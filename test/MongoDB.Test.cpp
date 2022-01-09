c6efcad5dec37140be7883093fff113ff7a63233
alisher_ashyrov <alisher_ashyrov@mail.ru>
2022-01-10 01:27:40 +0300

    feat: updated unit tests

--- test/MongoDB.Test.cpp
+++ test/MongoDB.Test.cpp
@@ -3,6 +3,7 @@
 //
 #include <MongoDB.hpp>
 #include <gtest/gtest.h>
+#include <util.hpp>
 
 TEST(User, MongoDB) {
     auto database = MongoDB::instance("127.0.0.1", "Test2");
@@ -59,4 +60,105 @@ TEST(User, MongoDB) {
     result = database->getUserById(newUser.id(), newUser);
     std::cerr << result.message << std::endl;
     ASSERT_FALSE(result.ok);
+}
+
+TEST(Test, MongoDB) {
+    std::vector<std::string> userIds;
+
+    auto database = MongoDB::instance("127.0.2.1", "Test2");
+    book::User user;
+    {
+        user.set_login("se7enrains");
+        user.set_name("Pete");
+        user.set_lastname("Ivanov");
+        user.set_email("se7enrains@mail.ru");
+        user.set_tel("1234556");
+        user.set_password("qwerty12345");
+        user.mutable_birthday()->set_month(1);
+        user.mutable_birthday()->set_day(1);
+        user.mutable_birthday()->set_year(1998);
+
+        std::string id;
+        auto result = database->addUser(user, id);
+        EXPECT_TRUE(result.ok);
+        userIds.push_back(id);
+    }
+    {
+        user.set_login("ofay54");
+        user.set_name("Kare");
+        user.set_lastname("Chick");
+        user.set_password("qwerty12345");
+        user.set_email("ofay54@mail.ru");
+        user.set_tel("1234567");
+        user.mutable_birthday()->set_month(1);
+        user.mutable_birthday()->set_day(1);
+        user.mutable_birthday()->set_year(2001);
+
+        std::string id;
+        auto result = database->addUser(user, id);
+        EXPECT_TRUE(result.ok);
+        userIds.push_back(id);
+    }
+    {
+        user.set_login("viki");
+        user.set_name("Vika");
+        user.set_lastname("Simo");
+        user.set_email("viki@mail.ru");
+        user.set_password("qwerty12345");
+        user.set_tel("98765321");
+        user.mutable_birthday()->set_month(1);
+        user.mutable_birthday()->set_day(1);
+        user.mutable_birthday()->set_year(1993);
+
+        std::string id;
+        auto result = database->addUser(user, id);
+        EXPECT_TRUE(result.ok);
+        userIds.push_back(id);
+    }
+    {
+        user.set_login("sashka");
+        user.set_name("Sasha");
+        user.set_lastname("Shido");
+        user.set_email("shido@mail.ru");
+        user.set_password("qwerty12345");
+        user.set_tel("187387342");
+        user.mutable_birthday()->set_month(1);
+        user.mutable_birthday()->set_day(1);
+        user.mutable_birthday()->set_year(1995);
+
+        std::string id;
+        auto result = database->addUser(user, id);
+        EXPECT_TRUE(result.ok);
+        userIds.push_back(id);
+    }
+    {
+        user.set_login("diasha");
+        user.set_name("Dia");
+        user.set_lastname("Biru");
+        user.set_email("dia@mail.ru");
+        user.set_tel("555555");
+        user.set_password("qwerty12345");
+        user.mutable_birthday()->set_month(12);
+        user.mutable_birthday()->set_day(22);
+        user.mutable_birthday()->set_year(1998);
+
+        std::string id;
+        auto result = database->addUser(user, id);
+        EXPECT_TRUE(result.ok);
+        userIds.push_back(id);
+    }
+
+    book::Users users;
+    auto result = database->getUsers(users);
+    std::cout << result.message << std::endl;
+    EXPECT_TRUE(result.ok);
+    EXPECT_FALSE(users.users().empty());
+
+    book::GetUsersListResponse response;
+    response.mutable_content()->CopyFrom(users);
+
+    std::cout << to_string(response) << std::endl;
+    for (const auto& id : userIds) {
+        database->removeUserById(id);
+    }
 }
